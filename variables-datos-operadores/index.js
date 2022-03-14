676a007f40ef5c2727ede5faea907e6396256a3f
NachiBasaldella <nazaretbasaldel@gmail.com>
2022-03-14 17:11:37 -0300

    commit uno

---
+++ variables-datos-operadores/index.js
@@ -0,0 +1,100 @@
+// ejercicio 1 Saludo
+// const nombre = prompt("Escriba su nombre")
+// const apellido = prompt("Escriba su apellido")
+// alert("Hola " + nombre + " " + apellido + ", bienvenido a Ada")
+
+// ejercicio 2 Heladería 
+// const primerGustoDeHelado = prompt("Primer gusto de helado")
+// const segundoGustoDeHelado = prompt("Segundo gusto de helado")
+// const tercerGustoDeHelado = prompt("Tercer gusto de helado")
+// alert("Aquí tiene su helado de " + primerGustoDeHelado + ", " + segundoGustoDeHelado + "y " + tercerGustoDeHelado + ".")
+
+
+// ejercicio 3 Datos Personales
+// const nombre = prompt("Escriba su nombre")
+// const apellido = prompt("Escriba su apellido")
+// const edad = prompt("Escriba su edad")
+// const nacionalidad = prompt("Escriba su nacionalidad")
+// const documento = prompt("Escriba su documento")
+// alert(`Nuevo usuario agregado al sistema:${nombre} ${apellido}; ${edad}; ${nacionalidad}; ${documento}.`)
+
+// ejercicio 4 Lista de reproducción
+// const playlist = prompt ("Escriba el nombre de una Playlist")
+// const canción1 = prompt ("Nombre de canción número 1")
+// const canción2 = prompt ("Nombre de canción número 2")
+// const canción3 = prompt ("Nombre de canción número 3")
+// alert ("Se ha creado la playlist " + "`" + playlist + "` " + "con las canciones " + canción1 + " " + canción2 + " " + canción3 + ".")
+
+// ejercicio 5 Dirección completa
+// const calle = prompt("Escriba su calle")
+// const número = prompt("Escriba número de calle")
+// const departamento = prompt("Escriba número de departamento")
+// const códigopostal = prompt("Escriba el número de código postal")
+// const ciudad = prompt("Escriba nombre de ciudad")
+// const país = prompt("Escriba el País donde vive")
+// alert("La dirección que ha ingresado es:" + calle + " " + número + " " + departamento + " " + códigopostal + " " + ciudad + " " + país + ".")
+
+// ejercicio 6 Años perro
+// const edad = prompt("Escriba su edad")
+// alert(Number(edad) * 7)
+
+// ejercicio 7 Minutos a segundos
+// const minutos = prompt("Escriba cantidad de minutos")
+// alert  = prompt(Number(minutos) * 60)
+
+// ejercicio 8 días a segundos 
+// const días = prompt("Escriba cantidad de días")
+// alert = prompt(Number(días) * 60 * 60 * 24)
+
+// ejercicio 9 Kilómetros a millas
+// const kilometros = prompt("Escriba cantidad de Kilometros")
+// alert = prompt(Number(kilometros) * 0.621371)
+
+// ejercicio 10 área de un triangulo 
+// const basedetriangulo = prompt("Escriba valor de base de triangulo")
+// const alturatriangulo = prompt("Escriba valor altura de triangulo")
+// alert = prompt(Number(basedetriangulo) * Number(alturatriangulo) / 2)
+
+//ejercicio 11 Perímetro de un rectángulo
+//const altura = prompt("Escriba valor de la altura del rectángulo")
+//const ancho = prompt("Escriba valor del ancho del rectángulo")
+//alert = prompt(Number(altura) * Number(ancho))
+
+// ejercicio 12 Porcentaje 
+// const número = prompt("Escriba número")
+// const porcentaje = prompt("Escriba porcentaje que desee obtener")
+// alert = prompt(Number(número) * Number(porcentaje) / 100)
+
+// ejercicio 13 Tiempo de viaje
+// const distanciaderecorrido = prompt("Escriba la distancia de su recorrido")
+//  alert = prompt("tiempo en pie: " + distanciaderecorrido / 5 + "; " + "tiempo en tren: " + distanciaderecorrido / 10 + "; " + "tiempo en colectivo: " + distanciaderecorrido / 8 + ".")
+
+// ejercicio 14 Duración vuelo
+// const destinouno = prompt("Escriba primera escala")
+// const duraciónuno = prompt("Escriba duración de primera escala")
+// const destinodos = prompt("Escrba segunda escala")
+// const duracióndos = prompt("Escriba duración de segunda escala")
+// const destinotres = prompt("Escrba tercer escala")
+// const duracióntres = prompt("Escriba duración de tercer escala")
+// alert = prompt(`${destinouno} ${duraciónuno} ${destinodos} ${duracióndos} ${destinotres} ${duracióntres}`)
+// // alert = prompt(Number(duraciónuno) + Number(duracióndos) + Number(duracióntres))
+
+// ejercicio 15 Incremento
+// const número = prompt("Escriba número")
+// const incremento = prompt("Escriba cantidad de incremento")
+// alert = prompt("Incremento 1)Total: " + (
+//     Number(número) + Number(incremento)))
+// alert = prompt("Incremento 2)Total: " + (
+//     Number(número) + Number(incremento) + Number(incremento)))
+// alert = prompt("Incremento 3)Total: " + (
+//     Number(número) + Number(incremento) + Number(incremento) + Number(incremento)))
+// alert = prompt("Incremento 4)Total: " + (
+//     Number(número) + Number(incremento) + Number(incremento) + Number(incremento) + Number(incremento)))
+// alert = prompt("Incremento 5)Total: " + (
+//     Number(número) + Number(incremento) + Number(incremento) + Number(incremento) + Number(incremento) + Number(incremento)))
+
+// ejercicio 16 Fahrenheit
+// const Celsius = prompt("escribi la cantidad de grados Celcius")
+// alert = prompt("Grados Fahrenheit" (Number(Celsius) + 32))
+
+// ejercicio 17 Múltiplos
