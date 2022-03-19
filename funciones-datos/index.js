a362c32cb2a636b832187236b9c544c9a5a3186e
NachiBasaldella <nazaretbasaldel@gmail.com>
2022-03-19 15:03:06 -0300

    subo todos los cambios el ejercicios de primera clase y tambien de clase 2 de funciones- datos

---
+++ funciones-datos/index.js
@@ -0,0 +1,86 @@
+//Funciones JS//
+//EJ .01//
+//const numero1 = prompt("ingrese el primer número")
+//const numero2 = prompt("ingrese el segundo número ")
+
+//function sumar(a, b) {
+//   const total = parseInt(a) + parseInt(b);
+//  return total}
+
+//const total = sumar (a, b)
+//console.log( "el total de la suma es:", total)
+// alert (`resultado: ${total}`)
+
+//EJ 02//
+// function restar(a, b) {
+//     const total = a - b;
+//     return total
+// }
+// const n1 = prompt("numero 1")
+// const n2 = prompt("numero 2")
+// const total = restar(n1, n2)
+// console.log("el total de la resta es :", total)
+// alert(`resultado: ${total}`)
+
+// EJ 03//
+// function multiplicar(a, b) {
+//     const total = a . b;
+//     return total
+// }
+// const n1 = prompt("numero 1")
+// const n2 = prompt("numero 2")
+// const total = restar(n1, n2)
+// console.log("el total de la multiplicación es :", total)
+// alert(`resultado: ${total}`)
+
+// EJ 04//
+// function dividir(a, b) {
+//     const total = a / b;
+//     return total
+// }
+// const n1 = prompt("numero 1")
+// const n2 = prompt("numero 2")
+// const total = restar(n1, n2)
+// console.log("el total de la división es :", total)
+// alert(`resultado: ${total}`)
+
+//EJ 05
+
+// const numero = 11
+
+// function esPar(numero) {
+//     if (numero % 2 == 0) {
+//         return true;
+//     } else {
+//         return false;
+
+//     }
+// }
+
+// const num = prompt("numero")
+// const resultado = esPar(num)
+// console.log(`el numero que pusiste es:`, resultado)
+
+// EJ 06
+// const numero = 11
+
+// function esImpar(numero) {
+//     if (numero % 2 == 1) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+
+// }
+// const num = prompt("numero")
+// const resultado = esImpar(num)
+// console.log(`el numero que pusiste es:`, resultado)
+
+// EJ 07
+
+function gritar(a) {
+    return a
+}
+const frase = prompt(`qué gritas?`)
+const resultado = gritar(frase)
+console.log(`el resultado de tu grito es:`, `¡`, resultado, `!`)
